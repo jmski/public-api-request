abfe8c00dcc9e62b010cc126e64792eb7079230c
Jon-Michael Gelua <geluajm@gmail.com>
2020-12-08 10:06:50 -0500

    made the gallery function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,52 @@
+const gallery = document.getElementById('gallery');
+
+
+
+// fetch API function
+function fetchData(url) {
+    return fetch(url)
+    .then(checkStatus)
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(data => {
+        const employees = data.results;
+        employees.forEach((employee) => {
+            createGallery(employee);
+        })
+    })
+}
+
+
+createSearchBar();
+
+
+Promise.all([
+fetchData('https://randomuser.me/api/?results=12&nat=us')
+])
+
+function checkStatus(response) {
+    if (response.ok) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+} 
+
+function createGallery(data) {
+    const galleryHTML = `               
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${data.picture.large}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
+                <p class="card-text">${data.email}</p>
+                <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
+            </div>
+        </div>`;
+    gallery.insertAdjacentHTML('beforeend', galleryHTML);
+}
+
 
 // this function creates the search bar
 function createSearchBar() {
@@ -9,40 +58,5 @@ function createSearchBar() {
         </form>`;
     const searchDiv = document.querySelector('.search-container');
     searchDiv.insertAdjacentHTML('beforeend', searchHTML);
-
-    // const searchInput = document.getElementById('#search-input');
-    // console.log(searchInput);
-
-    // searchInput.addEventListener('keyup', (event) => {
-    //     const input = e.target.value.toLowerCase();
-    //     results = [ ];
-    //     for ( let i = 0; i < Element.length; i++) {
-
-    //     }
-    // })
-}
-
-// fetch API function
-function fetchData(url) {
-    return fetch(url)
-    .then(checkStatus)
-    .then(response => response.json())
-    .catch(error => console.log('Looks like there was a problem', error))
-}
-
-Promise.all([
-fetchData('https://randomuser.me/api/?results=12&nat=us')
-])
-createSearchBar();
-
-function checkStatus(response) {
-    if (response.ok) {
-        return Promise.resolve(response);
-    } else {
-        return Promise.reject(new Error(response.statusText));
-    }
-} 
-
-function createModel(data) {
 
 }
