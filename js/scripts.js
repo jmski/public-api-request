433d0d342d1ab2b8011c910318a68fe1ba4c3138
Jon-Michael Gelua <geluajm@gmail.com>
2020-12-08 21:51:04 -0500

    working on search filter feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 // global variables
 const gallery = document.getElementById('gallery');
+const card = document.getElementsByClassName('card');
 
 // fetch API function
 function fetchData(url) {
     return fetch(url)
     .then(checkStatus)
     .then(response => response.json())
-    // .then(data => console.log(data)) // view data
+    //.then(data => console.log(data)) // view data
     .then(data => {
         const employees = data.results;
         employees.forEach((employee) => {
@@ -14,6 +15,7 @@ function fetchData(url) {
             createModalWindow(employee);
         })
         createSearchBar();
+        searchEmployee();
         eventListener();
     })
 }
@@ -33,13 +35,33 @@ function checkStatus(response) {
 
 // this function creates the search bar
 function createSearchBar() {
+    const searchDiv = document.querySelector('.search-container');
     const searchHTML = `
         <form action="#" method="get">
         <input type="search" id="search-input" class="search-input" placeholder="Search...">
         <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
         </form>`;
-    const searchDiv = document.querySelector('.search-container');
     searchDiv.insertAdjacentHTML('beforeend', searchHTML);
+}
+// this function matches employee to input
+function searchEmployee (data) {
+    const searchInput = document.querySelector('#search-input');
+    
+    searchInput.addEventListener('keyup', (event) => {
+        const input = event.target.value.toLowerCase();
+
+        for (let i = 0; i < card.length; i++) {
+            const employee = card[i].toLowerCase();
+            console.log(input);
+            if (employee.includes(input)) {
+                card[i].style.display = 'block';
+            } else if (employee.includes(input) === false) {
+                card[i].style.display = 'none';
+            } else {
+                card[i].style.display = 'block';
+            }
+        }
+    });
 }
 
 // creates the gallery of 12 people with their picture and info
@@ -47,7 +69,7 @@ function createGallery(data) {
     const galleryHTML = `               
         <div class="card">
             <div class="card-img-container">
-                <img class="card-img" src="${data.picture.large}" alt="profile picture">
+                <img class="card-img" src="${data.picture.large}" alt="${data.name.first} ${data.name.last}">
             </div>
             <div class="card-info-container">
                 <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
@@ -60,6 +82,12 @@ function createGallery(data) {
 
 // creates the modal window
 function createModalWindow(data) {
+
+    // formats the date for birth
+    const formatDob = (data.dob.date)
+        .replace( /\T(.*)$/, ' ')
+        .replace(/^(\d{4})-(\d{2})-(\d{2})*/, '$2/$3/$1');
+
     const modalDiv = document.createElement('div');
     modalDiv.className = 'modal-container';
     gallery.appendChild(modalDiv);
@@ -67,14 +95,14 @@ function createModalWindow(data) {
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src="${data.picture.large}" alt="profile picture">
+                <img class="modal-img" src="${data.picture.large}" alt="${data.name.first} ${data.name.last}">
                 <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
                 <p class="modal-text">${data.email}</p>
                 <p class="modal-text cap">${data.location.city}</p>
                 <hr>
                 <p class="modal-text">${data.phone}</p>
-                <p class="modal-text">${data.location.street.name}, ${data.location.state}, ${data.location.postcode}</p>
-                <p class="modal-text">Birthday: ${data.dob.date}</p>
+                <p class="modal-text">${data.location.street.name}, ${data.location.state} ${data.location.postcode}</p>
+                <p class="modal-text">Birthday: ${formatDob}</p>
             </div>
             <div class="modal-btn-container">
                 <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
@@ -87,7 +115,6 @@ function createModalWindow(data) {
 
 // this function opens/ closes the modal window on click and navigate through the cards
 function eventListener() {
-    const card = document.getElementsByClassName('card');
     const modalDiv = document.getElementsByClassName('modal-container');
     const closeBtn = document.getElementsByClassName('modal-close-btn');
     const buttons = document.getElementsByClassName('modal-btn-container');
